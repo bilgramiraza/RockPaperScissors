0abe8390aab2de82bce10ac59258789dcbd80471
Raza Hassan <bilgramiraza@yahoo.com>
2021-06-28 11:34:56 +0530

    Added Player Input

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function computerPlay(){
     const choice = Math.floor((Math.random()*3));
     return RPS[choice];
 }
+
+function playerPlay(){
+    let input = prompt('Rock, Paper or Scissors?');
+    choice=input.toUpperCase();
+    switch(choice){
+        case 'ROCK':return RPS[0];
+                    break;
+        case 'PAPER':return RPS[1];
+                    break;
+        case 'SCISSORS':return RPS[2];
+                    break;
+        default: return playerPlay();
+    }
+}
+console.log(playerPlay());
