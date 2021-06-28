60e0b717a7edd74ce93816e9e27b82a02db4d0e1
Raza Hassan <bilgramiraza@yahoo.com>
2021-06-28 12:24:06 +0530

    Added Win/Loss Condition

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const RPS= ["Rock", "Paper", "Scissors"];
+const wintable= [[0,2,1],
+                 [1,0,2],
+                 [2,1,0]];
 
 function computerPlay(){
-    const choice = Math.floor((Math.random()*3));
-    return RPS[choice];
+    const choice = Math.floor((Math.random()*3)); 
+    //console.log(`Computer: ${RPS[choice]}`);   //Error Handling
+    return choice;
 }
 
 function playerPlay(){
-    let input = prompt('Rock, Paper or Scissors?');
+    let input = prompt('Rock, Paper or Scissors?'); 
+    //console.log(`Player: ${RPS[input]}`);   //Error Handling
     choice=input.toUpperCase();
     switch(choice){
-        case 'ROCK':return RPS[0];
+        case 'ROCK':return 0;
                     break;
-        case 'PAPER':return RPS[1];
+        case 'PAPER':return 1;
                     break;
-        case 'SCISSORS':return RPS[2];
+        case 'SCISSORS':return 2;
                     break;
         default: return playerPlay();
     }
 }
-console.log(playerPlay());
+
+function playRound(computerChoice, playerChoice){
+    const result = wintable[playerChoice][computerChoice];
+    return result;
+}
