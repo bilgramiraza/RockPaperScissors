0bdce0105b7318a2f3ad189a2fa87c8a742a59ea
Raza Hassan <bilgramiraza@yahoo.com>
2021-07-04 17:57:51 +0530

    Changed WinCondition

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const RPS= ["Rock", "Paper", "Scissors"];
+//The Result for every combination of Input
 const wintable= [[0,2,1],
                  [1,0,2],
                  [2,1,0]];
 
+//Computer Input
 function computerPlay(){
     const choice = Math.floor((Math.random()*3)); 
     //console.log(`Computer: ${RPS[choice]}`);   //Error Handling
     return choice;
 }
 
+//Player Input
 function playerPlay(){
     let input = prompt('Rock, Paper or Scissors?'); 
     //console.log(`Player: ${RPS[input]}`);   //Error Handling
@@ -24,14 +27,18 @@ function playerPlay(){
     }
 }
 
+//Decision making
 function playRound(computerChoice, playerChoice){
     const result = wintable[playerChoice][computerChoice];
     return result;
 }
+
 function game(){
     let result=0;
+    let playerScore=0;
+    let computerScore=0
     let winner=0;
-    for(let i=0;i<5;i++){
+    while(playerScore !== 5 && computerScore !== 5){
         let computerChoice = computerPlay();
         let playerChoice = playerPlay();
         result = playRound(computerChoice,playerChoice);
@@ -39,18 +46,18 @@ function game(){
             case 0:console.log(`Its a Draw`);
                     break;
             case 1:console.log(`${RPS[playerChoice]} beats ${RPS[computerChoice]} Player Wins`);
-                    winner++;
+                    playerScore++;
                     break;
             case 2:console.log(`${RPS[computerChoice]} beats ${RPS[playerChoice]} Computer Wins`);
-                    winner--;
+                    computerScore++;
                     break;
         }
     }
-    if(!winner)
-        console.log('Its a Draw');
-    else if(winner>0)
+    if(playerScore===5)
         console.log('Player Wins')
-    else
+    else if(computerScore===5)
         console.log('Computer Wins');
+    else
+        console.log('Its a Draw');
 }
 game();
